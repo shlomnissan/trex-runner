539a67df6ce639fb08b78d870f7e7f2299c09bad
Shlomi Nissan <shlomi@betamark.com>
2021-04-19 17:06:25 -0700

    Add ducking state on down arrow key

--- actors/t_rex.c
+++ actors/t_rex.c
@@ -1,6 +1,8 @@
 // Copyright 2021 Betamark Pty Ltd. All rights reserved.
 // Author: Shlomi Nissan (shlomi@betamark.com)
 
+#include <stdio.h>
+
 #include "t_rex.h"
 
 #include "sdl/graphics.h"
@@ -11,7 +13,8 @@
 typedef enum {
     WAITING = 0,
     RUNNING,
-    JUMPING
+    JUMPING,
+    DUCKING,
 } TRexState;
 
 AnimFrame tx_anim[] = {
@@ -33,10 +36,17 @@ AnimFrame tx_anim[] = {
         .len = 1,
         .ms_per_frame = 1000 / 60
     },
+    {
+        // Ducking
+        .frames = {264, 323},
+        .len = 2,
+        .ms_per_frame = 1000 / 8
+    }
 };
 
 int tx_width = 44;
 int tx_height = 47;
+int tx_ducking_width = 59;
 int tx_ground_offset = 10;
 int tx_time = 0;
 int tx_curr_frame = 0;
@@ -55,8 +65,6 @@ void UpdateAnimationFrames(uint32_t delta_time);
 void StartJump();
 void UpdateJump(uint32_t delta_time);
 void Reset();
-
-int GroundPosition();
 
 bool IsJumpKeyPressed();
 
@@ -81,6 +89,12 @@ void UpdateTRex(uint32_t delta_time) {
 void HandleControls() {
     if (tx_state != JUMPING && IsJumpKeyPressed()) {
         StartJump();
+    }
+    if (tx_state == RUNNING && IsKeyPressed(KEY_DOWN)) {
+        UpdateState(DUCKING);
+    }
+    if (tx_state == DUCKING && !IsKeyPressed(KEY_DOWN)) {
+        Reset();
     }
 }
 
@@ -128,7 +142,7 @@ void Reset() {
 void UpdateAnimationFrames(uint32_t delta_time) {
     tx_time += delta_time;
     if (tx_time >= tx_anim_frame.ms_per_frame) {
-        if (tx_curr_frame == tx_anim_frame.len) {
+        if (tx_curr_frame == tx_anim_frame.len - 1) {
             tx_curr_frame = 0;
         } else {
             tx_curr_frame += 1;
@@ -138,18 +152,19 @@ void UpdateAnimationFrames(uint32_t delta_time) {
 }
 
 void DrawTRex() {
+    int width = tx_state != DUCKING ? tx_width : tx_ducking_width;
     Texture texture = {
         .id = 0,
         .source = {
             .x = tx_sprite_def.x + tx_anim_frame.frames[tx_curr_frame],
             .y = tx_sprite_def.y,
-            .width = tx_width,
+            .width = width,
             .height = tx_height
         },
         .destination = {
                 .x = tx_pos.x,
                 .y = tx_pos.y,
-                .width = tx_width,
+                .width = width,
                 .height = tx_height
         }
     };
