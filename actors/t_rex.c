f14513a176ae9646ed1c3a8d1156ad685b99658e
Shlomi Nissan <shlomi@betamark.com>
2021-04-20 08:38:14 -0700

    Improve jump function by adding speed drop

--- actors/t_rex.c
+++ actors/t_rex.c
@@ -11,8 +11,7 @@
 #include "globals.h"
 
 typedef enum {
-    WAITING = 0,
-    RUNNING,
+    RUNNING = 1,
     JUMPING,
     DUCKING,
 } TRexState;
@@ -53,6 +52,9 @@ int tx_curr_frame = 0;
 int tx_ground_pos = 0;
 
 double tx_jump_velocity = 0.0;
+
+bool tx_speed_drop = false;
+
 Point tx_sprite_def;
 Point tx_pos;
 
@@ -63,6 +65,8 @@ void UpdateState(TRexState state);
 void HandleControls();
 void UpdateAnimationFrames(uint32_t delta_time);
 void StartJump();
+void EndJump();
+void SetSpeedDrop();
 void UpdateJump(uint32_t delta_time);
 void Reset();
 
@@ -87,9 +91,18 @@ void UpdateTRex(uint32_t delta_time) {
 }
 
 void HandleControls() {
+    // Jumping
     if (tx_state != JUMPING && IsJumpKeyPressed()) {
         StartJump();
     }
+    if (tx_state == JUMPING && !IsJumpKeyPressed()) {
+        EndJump();
+    }
+    if (tx_state == JUMPING && IsKeyPressed(KEY_DOWN) && !tx_speed_drop) {
+        SetSpeedDrop();
+    }
+
+    // Ducking
     if (tx_state == RUNNING && IsKeyPressed(KEY_DOWN)) {
         UpdateState(DUCKING);
     }
@@ -118,14 +131,16 @@ void UpdateJump(uint32_t delta_time) {
     double ms_per_frame = tx_anim[tx_state].ms_per_frame;
     double frames_elapsed = delta_time / ms_per_frame;
 
-    tx_pos.y += tx_jump_velocity * frames_elapsed;
+    if (tx_speed_drop) {
+        tx_pos.y += tx_jump_velocity * SPEED_DROP_COEFFICIENT * frames_elapsed;
+    } else {
+        tx_pos.y += tx_jump_velocity * frames_elapsed;
+    }
 
     tx_jump_velocity += GRAVITY * frames_elapsed;
 
-    if (tx_pos.y < MAX_JUMP_HEIGHT) {
-        if (tx_jump_velocity < DROP_VELOCITY) {
-            tx_jump_velocity = DROP_VELOCITY;
-        }
+    if (tx_pos.y < MAX_JUMP_HEIGHT || tx_speed_drop) {
+        EndJump();
     }
 
     if (tx_pos.y > tx_ground_pos) {
@@ -133,9 +148,21 @@ void UpdateJump(uint32_t delta_time) {
     }
 }
 
+void EndJump() {
+    if (tx_jump_velocity < DROP_VELOCITY) {
+        tx_jump_velocity = DROP_VELOCITY;
+    }
+}
+
+void SetSpeedDrop() {
+    tx_speed_drop = true;
+    tx_jump_velocity = 1;
+}
+
 void Reset() {
     tx_pos.y = tx_ground_pos;
     tx_jump_velocity = 0;
+    tx_speed_drop = false;
     UpdateState(RUNNING);
 }
 
